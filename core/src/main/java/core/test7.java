d581c7089a2cb45ba6cbb689d99cec3584c579ac
Guy Knaan <guyknaan@gmail.com>
2020-09-25 20:37:01 +0300

    first commit

---
+++ core/src/main/java/core/test7.java
@@ -0,0 +1,8 @@
+package core;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class test7 extends ApplicationAdapter {
+
+}
---
+++ desktop/src/main/java/core/desktop/DesktopLauncher.java
@@ -0,0 +1,31 @@
+package core.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import core.test7;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static LwjglApplication createApplication() {
+		return new LwjglApplication(new test7(), getDefaultConfiguration());
+	}
+
+	private static LwjglApplicationConfiguration getDefaultConfiguration() {
+		LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+		configuration.title = "test7";
+		configuration.width = 640;
+		configuration.height = 480;
+		//// This prevents a confusing error that would appear after exiting normally.
+		configuration.forceExit = false;
+		
+		for (int size : new int[] { 128, 64, 32, 16 }) {
+			configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+		}
+		return configuration;
+	}
+}
